2977ae66c6bf15a088ce366de1aa96bf65d68a03
Dominik Sawallisch <dominik.sawallisch@hotmail.de>
2024-01-16 12:03:14 +0100

    fg

--- Python-Solidity-Connection-Test.py
+++ Python-Solidity-Connection-Test.py
@@ -1,2 +1,10 @@
-connection = Web3(HTTPProvider('https://mainnet.infura.io/v3/ae6ebad4a80b46299b5ea14e4d506af9'))
-print ("Latest Ethereum block number", connection.eth.blockNumber)
+
+# install Web3 with command pip install web3 first
+# set up infura api
+from web3 import Web3, HTTPProvider
+connection = Web3(HTTPProvider('https://goerli.infura.io/v3/ae6ebad4a80b46299b5ea14e4d506af9'))
+
+
+# get latest ethereum block number through infura
+print ("Latest Ethereum block number", connection.eth.block_number)
+
